76118b4cc1f123445db276d6d15abc9e99d78d80
roipk <roipk123@gmail.com>
2019-05-07 22:58:05 +0300

    Add full project
    Uploading the entire project so that we can run it from anywhere without installing additional plugins for a new user to arrive

--- Pi/src/environments/environment.prod.ts
+++ Pi/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCmDojaolOW114iVoCmUbrU6zAnPubeCA8",
+ authDomain: "parkour-israel.firebaseapp.com",
+ databaseURL: "https://parkour-israel.firebaseio.com",
+ projectId: "parkour-israel",
+ storageBucket: "parkour-israel.appspot.com",
+ messagingSenderId: "940612649818",
+ appId: "1:940612649818:web:d76ac9dc09b0d3fa"
+ }
 };
--- Pi/src/environments/environment.ts
+++ Pi/src/environments/environment.ts
@@ -3,8 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCmDojaolOW114iVoCmUbrU6zAnPubeCA8",
+    authDomain: "parkour-israel.firebaseapp.com",
+    databaseURL: "https://parkour-israel.firebaseio.com",
+ projectId: "parkour-israel",
+ storageBucket: "parkour-israel.appspot.com",
+ messagingSenderId: "940612649818",
+ appId: "1:940612649818:web:d76ac9dc09b0d3fa"
+ }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
