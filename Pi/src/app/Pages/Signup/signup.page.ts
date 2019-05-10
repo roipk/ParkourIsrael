d3588d7c9880b3f4d27d8eb2685637de33c82792
Roi Madmon <roipk123@gmail.com>
2019-05-10 18:41:14 +0300

    update firebse data users
    for every user saved email, first and last name
     and his nick name on firebase data

--- Pi/src/app/Pages/Signup/signup.page.ts
+++ Pi/src/app/Pages/Signup/signup.page.ts
@@ -10,10 +10,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage {
-
-  @ViewChild('fullName') fullNameField
+  @ViewChild('firstName') firstNameField
+  @ViewChild('lastName') lastNameField
+  @ViewChild('nickName') nickNameField
   @ViewChild('email') emailField
   @ViewChild('password') passField
+  @ViewChild('confirm') confirmField
 
   loadingRef = null
 
@@ -24,18 +26,43 @@ export class SignupPage {
     private router: Router) { }
 
   createNewUser(): void {
-    const fullName = this.fullNameField.value
     const email = this.emailField.value
     const password = this.passField.value
-    this.presentLoading()
-    this.userAuth.auth.createUserWithEmailAndPassword(email, password)
-    .then((result) => {
-      this.db.collection('users').doc(result.user.uid).set({ fullName: fullName })
-      .then(() => {
-        this.dismissLoading()
-        this.router.navigateByUrl('/news')
+    const firstName = this.firstNameField.value
+    const lastName = this.lastNameField.value
+    const fullName = firstName + ' ' + lastName
+    const nickName = this.nickNameField.value
+    const confirm = this.confirmField.value 
+    
+
+
+
+    if(fullName =='' || nickName =='' )
+    {
+      alert('one or more values empty' )
+    }
+    else if(password.length < 8 ){
+      alert('the password need to be 8 characters:\n'+
+      '*One or more big letter (A-Z)\n'+
+      '*One or more small letter (a-z)\n'+
+      '*One or more sign (@!%)\n'+
+      '*One or more number (0-9)\n')
+    }
+    else if(confirm != password)
+    {
+      alert('no mach password')
+    }
+    else{
+      this.presentLoading()
+      this.userAuth.auth.createUserWithEmailAndPassword(email, password)
+      .then((result) => {
+        this.db.collection('users').doc(result.user.uid).set({ fullName: fullName, email: email, nickName: nickName })
+        .then(() => {
+          this.dismissLoading()
+          this.router.navigateByUrl('/news')
+        })
       })
-    })
+    }
   }
 
   async presentLoading() {
