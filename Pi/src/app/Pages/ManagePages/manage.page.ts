79cdefec25d3629899cc8cb047ee4209ed8121f6
Roi Madmon <roipk123@gmail.com>
2019-05-20 02:19:46 +0300

    get manager users and post from db

--- Pi/src/app/Pages/ManagePages/manage.page.ts
+++ Pi/src/app/Pages/ManagePages/manage.page.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
+import { post } from 'selenium-webdriver/http';
 
 
 
@@ -11,51 +12,100 @@ import { Router } from '@angular/router';
   styleUrls: ['manage.page.scss'],
 })
 export class ManagePage {
-  manager = false;
-  pages = false;
-  posts = false;
-  users = false;
 
+  @ViewChild('title') titleField
+  @ViewChild('date') dateField
+
+
+  manager = false;
+  showPages = false;
+  showPosts = false;
+  showUsers = false;
+  public pages = []
+  public posts = []
+  public users = []
+
+  
+
+
+  //{ url: '1', name: 'test' }
   constructor(
-    private uAuth:AngularFireAuth,
+    private uAuth: AngularFireAuth,
     private router: Router,
     private db: AngularFirestore
-    ) { }
+  ) { }
 
   ngOnInit(): void {
-    this.uAuth.user.subscribe(() => { 
-      if(this.uAuth.auth.currentUser != null)
+    this.db.collection('users').valueChanges().subscribe(
+      result => {
+        result.sort((m1, m2) => {
+          if (m1['userName'] > m2['userName']) return 1
+          else return -1
+        })
+        this.users = [...result]
+      })
+
+    this.db.collection('messages').valueChanges().subscribe(
+      result => {
+        result.sort((m1, m2) => {
+          if (m1['userName'] < m2['userName']) return 1
+          else return -1
+        })
+        this.posts = [...result]
+      })
+  }
+
+  serch(list, find) {
+    if (list == this.posts) {
+      if(find=='')
       {
-          this.db.collection('users').doc(this.uAuth.auth.currentUser.uid)
-          .get().subscribe(result => {
-          this.manager = result.data().manager  
-          if(!this.manager)
-          {
-            this.router.navigateByUrl('/home')
-          } 
+        this.db.collection('messages').valueChanges().subscribe(
+          result => {
+            result.sort((m1, m2) => {
+              if (m1['timestamp'] < m2['timestamp']) return 1
+              else return -1
+            })
+            this.posts = [...result]
           })
+          return
       }
-      else{
-        this.router.navigateByUrl('/home')
-      } 
-    
-    })
+      this.posts = [...list.filter(obj => {
+        return obj.title === find
+      })]
+    }
+
+    if(list == this.users)
+    {
+
+      if(find=='')
+      {
+        this.db.collection('users').valueChanges().subscribe(
+          result => {
+            result.sort((m1, m2) => {
+              if (m1['userName'] > m2['userName']) return 1
+              else return -1
+            })
+            this.users = [...result]
+          })
+          return
+      }
+      this.users = [...list.filter(obj => {
+        return obj.userName === find
+      })]
+    }
   }
 
-  openPagesList()
-  {
-    this.pages = !this.pages;
+  openPagesList() {
+    this.showPages = !this.showPages;
   }
 
-  openPostsList()
-  {
-    this.posts = !this.posts;
+  openPostsList() {
+    this.showPosts = !this.showPosts;
   }
 
-  openUsersList()
-  {
-    this.users = !this.users;
+  openUsersList() {
+    this.showUsers = !this.showUsers;
   }
 }
-  
+
 
