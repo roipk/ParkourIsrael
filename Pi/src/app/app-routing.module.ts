539c2f6a951d2ec4ddd79c2a3cd5bc168dd1df74
Lyaure <lyauresebbah@gmail.com>
2019-05-26 15:40:21 +0300

    Auto stash before merge of "master" and "origin/master"

--- Pi/src/app/app-routing.module.ts
+++ Pi/src/app/app-routing.module.ts
@@ -13,12 +13,8 @@ const routes: Routes = [
   { path: 'info', loadChildren: './Pages/Info/info.module#InfoPageModule' },
   { path: 'manager', loadChildren: './Pages/ManagePages/manage.module#ManagePageModule', canActivate: [ IsManagerGuard ] },
   { path: 'writPost', loadChildren: './Pages/post-editor/post-editor.module#PostEditorPageModule',  canActivate: [ IsManagerGuard ] },
-  // { path: 'maps', loadChildren: './App/Pages/maps/maps.module#MapsPageModule' },
-  // { path: 'maps', loadChildren: './app/Pages/maps/maps.module#MapsPageModule' },
-  // { path: 'maps', loadChildren: './Pages/maps/maps.module#MapsPageModule' },
-  // { path: 'maps', loadChildren: './app/Pages/maps/maps.module#MapsPageModule' },
-
- 
+  { path: 'contact', loadChildren: './Pages/contact/contact.module#ContactPageModule' },
+  { path: '**', loadChildren: './Pages/not-found/not-found.module#NotFoundPageModule' }, 
 ];
 
 
