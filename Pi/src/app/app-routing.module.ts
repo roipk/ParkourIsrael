6ff7310167375c6178ee50c8f7263751487519e8
Roi Madmon <roipk123@gmail.com>
2019-05-26 10:53:59 +0300

    maps

--- Pi/src/app/app-routing.module.ts
+++ Pi/src/app/app-routing.module.ts
@@ -13,6 +13,11 @@ const routes: Routes = [
   { path: 'info', loadChildren: './Pages/Info/info.module#InfoPageModule' },
   { path: 'manager', loadChildren: './Pages/ManagePages/manage.module#ManagePageModule', canActivate: [ IsManagerGuard ] },
   { path: 'writPost', loadChildren: './Pages/post-editor/post-editor.module#PostEditorPageModule',  canActivate: [ IsManagerGuard ] },
+  // { path: 'maps', loadChildren: './App/Pages/maps/maps.module#MapsPageModule' },
+  // { path: 'maps', loadChildren: './app/Pages/maps/maps.module#MapsPageModule' },
+  // { path: 'maps', loadChildren: './Pages/maps/maps.module#MapsPageModule' },
+  // { path: 'maps', loadChildren: './app/Pages/maps/maps.module#MapsPageModule' },
+
  
 ];
 
