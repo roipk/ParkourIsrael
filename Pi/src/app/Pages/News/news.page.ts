97350eb5e8a3fde710e43d027bf80ab0c7e2cb99
Arie <arieakouka@hotmail.com>
2019-05-30 20:38:16 +0300

    Add option to filter posts
    -display all posts
    -display just user posts

--- Pi/src/app/Pages/News/news.page.ts
+++ Pi/src/app/Pages/News/news.page.ts
@@ -17,7 +17,10 @@ import * as firebase from 'firebase';
 export class NewsPage {
 
   [x: string]: any;
+
   fullName = ''
+  email = ''
+
   messages = []
 
 
@@ -27,11 +30,28 @@ export class NewsPage {
     // private ngZone: NgZone,
     private route: Router,
     private db: AngularFirestore,
-    private guard: IsManagerGuard
+    private guard: IsManagerGuard,
   ) { }
 
 
+  ngOnInit() {
+    this.uAuth.user.subscribe(() => {
+      this.getPostsFromDb('allPosts')
+      if(this.uAuth.auth.currentUser != null) {
+        this.afterUserInside()
+      }
+    })
+  }
 
+  async afterUserInside() {
+    await this.db.collection('users').doc(this.uAuth.auth.currentUser.uid)
+      .get().subscribe(result => {
+        this.email = result.data().email
+      })
+  }
+
+
+  /*
   ngOnInit(): void {
     this.uAuth.user.subscribe(() => {
     })
@@ -44,6 +64,31 @@ export class NewsPage {
         this.messages = [...result]
         return
       })
+  }
+  */
+
+  getPostsFromDb(param: string): void {
+    if (param == 'allPosts') {
+      this.db.collection('messages').valueChanges().subscribe(
+        result => {
+          result.sort((m1, m2) => {
+            if (m1['timestamp'] < m2['timestamp']) return 1
+            else return -1
+          })
+          this.messages = [...result]
+        })
+    }
+    else if (param == 'myPosts') {
+      this.db.collection('messages', ref => ref.where('email', '==', this.email)).valueChanges().subscribe(
+        result => {
+          result.sort((m1, m2) => {
+            if (m1['timestamp'] < m2['timestamp']) return 1
+            else return -1
+          })
+          this.messages = [...result]
+        }
+      )
+    }
   }
 
   createExit() {
@@ -59,6 +104,15 @@ export class NewsPage {
   getContentColor(m) {
     if (this.fullName != null && m != null && this.fullName === m.from) {
       return 'red'
+    }
+  }
+
+  radioButtonEvent(e) {
+    if (e.target.value == 'allPosts') {
+      this.getPostsFromDb('allPosts')
+    }
+    else if (e.target.value == 'myPosts') {
+      this.getPostsFromDb('myPosts')
     }
   }
 
