003c6adbcfbb1acebba6d0c172b852a2346420fe
Roi Madmon <roipk123@gmail.com>
2019-06-18 03:14:47 +0300

    home images slide
    image change after 10 sec or left/right buttons
    or down circle click

--- Pi/src/app/Pages/Home/home.page.ts
+++ Pi/src/app/Pages/Home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { Component, Input, ViewChild, ChangeDetectorRef, Renderer2 } from '@angular/core';
 // import { NavbarComponent } from '../navbar/navbar.component';
 
 
@@ -16,38 +16,47 @@ import { AngularFirestore } from '@angular/fire/firestore';
 export class HomePage {
 
   @ViewChild('generalTitle') generalTitle
-
+  @ViewChild('test') test
   fullName = '1';
   lan = true;
-  message=[]
-  massage1:any
-  massage2:any
-  massage3:any
+  public dots = [{
+    src: 'https://static.wixstatic.com/media/3f6e79_3e7ef9b2a01643afbb6b76d253a1120c~mv2.jpg/v1/fill/w_1500,h_628,al_c,q_90/3f6e79_3e7ef9b2a01643afbb6b76d253a1120c~mv2.webp'
+  },
+  {
+    src: 'https://static.wixstatic.com/media/3f6e79_edffb7457e594159ad8860143c48f0cd~mv2.jpg/v1/fill/w_1203,h_640,al_c,q_90,usm_0.66_1.00_0.01/3f6e79_edffb7457e594159ad8860143c48f0cd~mv2.webp'
+  }
+  ]
+  message = []
+  slideIndex = 0;
+
+  timeLeft: number = 10;
+  interval;
 
   constructor(
-    private cdRef : ChangeDetectorRef,
+    private cdRef: ChangeDetectorRef,
     private db: AngularFirestore,
-    
+    private renderer: Renderer2,
+
   ) { }
 
 
   ngOnInit(): void {
+    // this.showSlides(this.slideIndex);
     this.db.collection('messages').valueChanges().subscribe(
       result => {
         result.sort((m1, m2) => {
           if (m1['timestamp'] < m2['timestamp']) return 1
           else return -1
         })
-        for (let i = 0; i <3; i++) {
+        for (let i = 0; i < 3; i++) {
           this.message[i] = result[i]
-          
+
         }
         // this.message = [...result]
       })
-
-  //    this.massage1=this.message[0]
-  //    this.massage2 =this.message[0]
-  // this.massage3 =this.message[0]
+    this.test.src=this.dots[0].src
+    this.startTimer()
+   
   }
 
 
@@ -61,14 +70,53 @@ export class HomePage {
 
   }
 
-  isShowExpand()
-  {
+  isShowExpand() {
     return LanguageComponent.lan
   }
   lang() {
     return this.lan
-    
+
   }
 
+
+
+
+
+ 
+
+startTimer() {
+    this.interval = setInterval(() => {
+      if(this.timeLeft > 0) {
+        this.timeLeft--;
+      } else {
+        this.timeLeft = 10;
+        this.plusSlides(1)
+        
+      }
+    },1000)
+  }
+
+
+
+
+  plusSlides(n) {
+    this.timeLeft = 10;
+  this.slideIndex +=n
+  if(this.slideIndex>=this.dots.length)
+    this.slideIndex=0;
+  else if(this.slideIndex < 0)
+    this.slideIndex=this.dots.length-1;
+    this.showSlides(this.slideIndex);
+  }
+
+  showSlides(n) {
+    this.test.src=this.dots[n].src
+  }
+
+  imageSlides(n)
+  {
+    this.timeLeft = 10;
+    this.test.src=n.src
+  }
 
 }
