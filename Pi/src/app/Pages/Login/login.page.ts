ca9f2bec26167b63cbe3523b8c27d7ea3ef17211
Arie <arieakouka@hotmail.com>
2019-06-27 20:16:24 +0300

    Email verification

--- Pi/src/app/Pages/Login/login.page.ts
+++ Pi/src/app/Pages/Login/login.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, NgZone,Input, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ViewChild, NgZone, Input, ChangeDetectorRef } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { LoadingController, AlertController, ActionSheetController } from '@ionic/angular';
 import { Router } from '@angular/router';
@@ -31,6 +31,7 @@ export class LoginPage {
   email = ''
   SignIn = true
   lan = true
+  sendVerificationMail = false
   uid = this.afAuth.authState.pipe(
     map(authState => {
       if (!authState) {
@@ -42,7 +43,7 @@ export class LoginPage {
       authState.uid
     })
   );
-  
+
 
   constructor(
     private afAuth: AngularFireAuth,
@@ -54,7 +55,7 @@ export class LoginPage {
     private uAuth: AngularFireAuth,
     private alertController: AlertController,
     private cdRef: ChangeDetectorRef,
-) { }
+  ) { }
 
   ngOnInit(): void {
     this.uAuth.user.subscribe(() => {
@@ -62,29 +63,37 @@ export class LoginPage {
   }
 
   signInUser() {
-    
     const password = this.passField.value
     if (this.email && password && this.SignIn) {
       this.presentLoading()
       this.userAuth.auth.signInWithEmailAndPassword(this.email, password)
         .then((result) => {
-          this.dismissLoading()
-          // don't need this line
-          //this.ngZone.run(() => {  this.router.navigateByUrl('/news') })
-          this.router.navigateByUrl('/home')
-        
+          if (!result.user.emailVerified) {
+            this.userAuth.auth.signOut()
+            this.dismissLoading()
+            this.alertMailConfirmation(result, 'Please first confirm your email address')
+          }
+          else {
+            this.db.collection('users', ref => ref.where('email', '==', this.email)).get().subscribe(result => {
+              debugger
+              let id = result.docs[0].id
+              this.db.collection('users').doc(id).update({
+                emailVerified: true
+              })
+            })
+            this.dismissLoading()
+            this.router.navigateByUrl('/home')
+          }
         }).catch(() => {
           this.dismissLoading()
           //alert('Email or password not are correct')
           this.simpleAlert('Email or password are not correct')
         })
     }
-    /*
     else {
       //alert('Email or password are not correct')
       this.simpleAlert('Email or password are not correct')
     }
-    */
   }
 
 
@@ -98,18 +107,25 @@ export class LoginPage {
 
 
   async presentLoading() {
-    this.loadingRef = await this.loadingController.create({ message: 'Please wait...', })
+    let msg = ''
+    let msgEng = 'Please wait...'
+    let msgHeb = 'אנא המתינו...'
+    if (this.lan)
+      msg = msgEng
+    else
+      msg = msgHeb
+    this.loadingRef = await this.loadingController.create({ message: msg, })
     await this.loadingRef.present()
   }
 
-  
+
   dismissLoading() {
     this.loadingRef.dismiss()
   }
 
 
   CheckUser(user) {
-    
+
     this.db.collection('users', ref => ref.where('email', '==', user)).get().subscribe(result => {
       if (result.empty) {
         this.emailExsist = false
@@ -139,116 +155,156 @@ export class LoginPage {
     this.SignIn = false
   }
 
-passwordType: string = 'password';
-passwordIcon: string = 'eye-off';
+  passwordType: string = 'password';
+  passwordIcon: string = 'eye-off';
 
- hideShowPassword() {
-     this.passwordType = this.passwordType === 'text' ? 'password' : 'text';
-     this.passwordIcon = this.passwordIcon === 'eye-off' ? 'eye' : 'eye-off';
- }
+  hideShowPassword() {
+    this.passwordType = this.passwordType === 'text' ? 'password' : 'text';
+    this.passwordIcon = this.passwordIcon === 'eye-off' ? 'eye' : 'eye-off';
+  }
 
 
-isPasswordInEmpty(): boolean {
-  return this.passField == null || this.passField.value == null || this.passField.value.length <= 0
-}
+  isPasswordInEmpty(): boolean {
+    return this.passField == null || this.passField.value == null || this.passField.value.length <= 0
+  }
 
-isResetMailInEmpty(strMail: string): boolean {
-  return strMail.length <= 0
-}
+  isResetMailInEmpty(strMail: string): boolean {
+    return strMail.length <= 0
+  }
 
-async simpleAlert(msg: string) {
-  const alert = await this.alertController.create({
-    message: msg,
-    buttons: [
-      {
-        text: 'OK'
-      }]
+  async simpleAlert(msg: string) {
+    const alert = await this.alertController.create({
+      message: msg,
+      buttons: [
+        {
+          text: 'OK'
+        }]
     });
     await alert.present();
-}
+  }
 
-async alertPassword() {
-  const alert = await this.alertController.create({
-    header: 'Reset your password',
-    inputs: [
-      {
-        name: 'email',
-        placeholder: 'Please enter your email'
-         
-      }],
-    buttons: [
-      {
-        text: 'Send',
-        handler: async data => {
-          let msg = ''
-          if(this.isResetMailInEmpty(data.email)) {
-            msg = 'Email field is empty'
+  async alertMailConfirmation(result: auth.UserCredential, msg: string) {
+    const alert = await this.alertController.create({
+      message: msg,
+      buttons: [
+        {
+          text: 'Resend confirmation mail',
+          handler: async data => {
+            //await result.user.sendEmailVerification
+            await this.SendVerificationMail(result)
+            let msg2 = ''
+            if (this.sendVerificationMail)
+              msg2 = 'The mail has been sent'
+            else
+              msg2 = 'An error occured'
+            const alert2 = await this.alertController.create({
+              message: msg2,
+              buttons: [
+                {
+                  text: 'OK',
+                }]
+            });
+            await alert2.present();
           }
-          else {
-            await this.passwordReset(data.email);
-            if (this.emailSent) {
-              msg = 'Email sent successfully'
+        }
+        , {
+          text: 'Ok'
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  // Send email verfificaiton when new user sign up
+  async SendVerificationMail(result: auth.UserCredential) {
+    return await result.user.sendEmailVerification()
+      .then(() => {
+        this.sendVerificationMail = true
+      }).catch(() => {
+        this.sendVerificationMail = false
+      })
+  }
+
+  async alertPassword() {
+    const alert = await this.alertController.create({
+      header: 'Reset your password',
+      inputs: [
+        {
+          name: 'email',
+          placeholder: 'Please enter your email'
+
+        }],
+      buttons: [
+        {
+          text: 'Send',
+          handler: async data => {
+            let msg = ''
+            if (this.isResetMailInEmpty(data.email)) {
+              msg = 'Email field is empty'
             }
             else {
-              msg = 'Email is not valid'
+              await this.passwordReset(data.email);
+              if (this.emailSent) {
+                msg = 'Email sent successfully'
+              }
+              else {
+                msg = 'Email is not valid'
+              }
             }
+            const alert2 = await this.alertController.create({
+              message: msg,
+              buttons: [
+                {
+                  text: 'OK',
+                }]
+            });
+            await alert2.present();
           }
-          const alert2 = await this.alertController.create({
-            message: msg,
-            buttons: [
-              {
-                text: 'OK',
-              }]
-          });
-          await alert2.present();
         }
-      }
-    ]
-  });
-  await alert.present().then(() => {
-    const firstInput: any = document.querySelector('ion-alert input');
-    firstInput.focus();
-    KeyboardEvent
-    return;
-  });
-}
-
-async passwordReset(email: string) {
-  await this.userAuth.auth.sendPasswordResetEmail(email).then( ()=>{
-    this.emailSent = true
-  }).catch(() => {
-    this.emailSent = false
-  });   
+      ]
+    });
+    await alert.present().then(() => {
+      const firstInput: any = document.querySelector('ion-alert input');
+      firstInput.focus();
+      KeyboardEvent
+      return;
+    });
   }
 
-onKeyUp(data) {
-  data.keyCode
-  const ENTER_KEY_CODE = 13
-  if (data.keyCode === ENTER_KEY_CODE) {
-    this.signInUser()
-  }
-}
-
-
-
-
-    
-ngAfterViewChecked() {
-  let show = this.isShowExpand();
-  if (show != this.lan) { // check if it change, tell CD update view
-    this.lan = show;
-    this.cdRef.detectChanges();
+  async passwordReset(email: string) {
+    await this.userAuth.auth.sendPasswordResetEmail(email).then(() => {
+      this.emailSent = true
+    }).catch(() => {
+      this.emailSent = false
+    });
   }
 
-}
-
-isShowExpand() {
-  return LanguageComponent.lan
-}
-lang() {
-  return this.lan
-
-}
+  onKeyUp(data) {
+    data.keyCode
+    const ENTER_KEY_CODE = 13
+    if (data.keyCode === ENTER_KEY_CODE) {
+      this.signInUser()
+    }
+  }
 
 
+
+
+
+  ngAfterViewChecked() {
+    let show = this.isShowExpand();
+    if (show != this.lan) { // check if it change, tell CD update view
+      this.lan = show;
+      this.cdRef.detectChanges();
+    }
+
+  }
+
+  isShowExpand() {
+    return LanguageComponent.lan
+  }
+  lang() {
+    return this.lan
+
+  }
 }
--- Pi/src/app/Pages/Signup/signup.page.ts
+++ Pi/src/app/Pages/Signup/signup.page.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { LoadingController } from '@ionic/angular';
@@ -42,6 +43,7 @@ export class SignupPage {
     private db: AngularFirestore,
     private loadingController: LoadingController,
     private router: Router,
+    private alertController: AlertController,
     private uAuth: AngularFireAuth,
     private cdRef: ChangeDetectorRef,
   ) { }
@@ -54,8 +56,8 @@ export class SignupPage {
     })
   }
 
-//=============================== create new user===========================================//
-//==========================================================================================//
+  //=============================== create new user===========================================//
+  //==========================================================================================//
   createNewUser(): void {
     const email = this.emailField.value
     const password = this.passField.value
@@ -64,6 +66,8 @@ export class SignupPage {
     const fullName = firstName + ' ' + lastName
     const userName = this.userNameField.value
     const confirm = this.confirmField.value
+    const emailVerified = false
+    const timestamp = new Date().getTime()
     const imageProfile = "https://firebasestorage.googleapis.com/v0/b/parkour-israel.appspot.com/o/images%2Favatar.jpg?alt=media&token=ec1dfd38-fa0d-4f73-a953-51e2c7756f5f"
 
 
@@ -85,11 +89,12 @@ export class SignupPage {
       if (this.emailEmpty && this.userEmpty) {
         this.presentLoading()
         this.userAuth.auth.createUserWithEmailAndPassword(email, password)
-
           .then((result) => {
-            this.db.collection('users').doc(result.user.uid).set({ fullName: fullName, email: email, userName: userName,imageProfile: imageProfile ,firstName : firstName,  lastName : lastName})
+            this.db.collection('users').doc(result.user.uid).set({ fullName: fullName, email: email, userName: userName, imageProfile: imageProfile, firstName: firstName, lastName: lastName, emailVerified: emailVerified, timestamp: timestamp, date: this.postDate()[1] })
               .then(() => {
+                this.userAuth.auth.signOut()
                 this.dismissLoading()
+                this.SendVerificationMail()
                 this.router.navigateByUrl('/login')
               })
           })
@@ -100,10 +105,34 @@ export class SignupPage {
       }
     }
   }
-//==========================================================================================//
 
-  
-  
+  // Send email verfificaiton when new user sign up
+  async SendVerificationMail() {
+    return await this.userAuth.auth.currentUser.sendEmailVerification()
+      .then(() => {
+        //this.router.navigate(['<!-- enter your route name here -->']);
+        //alert("A verification mail have been sent to your mail box.\nPlease click on the link to confirm your registration.");
+        this.simpleAlert("A verification mail have been sent to your mail box.\nPlease click on the link to confirm your registration.");
+      }).catch(() => {
+        //alert("An error occured sending confirmation mail");
+        this.simpleAlert("An error occured sending confirmation mail")
+      })
+  }
+  //==========================================================================================//
+
+
+  async simpleAlert(msg: string) {
+    const alert = await this.alertController.create({
+      message: msg,
+      buttons: [
+        {
+          text: 'OK'
+        }]
+    });
+    await alert.present();
+  }
+
+
   CheckUsername() {
     this.db.collection('users', ref => ref.where('userName', '==', this.userNameField.value)).get().subscribe(result => {
       if (result.empty) {
@@ -115,9 +144,9 @@ export class SignupPage {
       }
     })
   }
-  
+
   CheckEmail() {
-    
+
     this.db.collection('users', ref => ref.where('email', '==', this.emailField.value)).get().subscribe(result => {
       if (result.empty) {
         this.emailEmpty = true
@@ -128,28 +157,28 @@ export class SignupPage {
       }
     })
   }
-  
-  
-    onKeyUp(data) {
-      const ENTER_KET_CODE = 13
-      if (data.keyCode === ENTER_KET_CODE) {
-        this.createNewUser()
-      }
+
+
+  onKeyUp(data) {
+    const ENTER_KET_CODE = 13
+    if (data.keyCode === ENTER_KET_CODE) {
+      this.createNewUser()
     }
-  
-    async presentLoading() {
-      this.loadingRef = await this.loadingController.create({ message: 'Please wait...', })
-      await this.loadingRef.present()
-    }
-  
-    dismissLoading() {
-      this.loadingRef.dismiss()
-    }
+  }
+
+  async presentLoading() {
+    this.loadingRef = await this.loadingController.create({ message: 'Please wait...', })
+    await this.loadingRef.present()
+  }
+
+  dismissLoading() {
+    this.loadingRef.dismiss()
+  }
 
 
 
 
-    
+
   ngAfterViewChecked() {
     let show = this.isShowExpand();
     if (show != this.lan) { // check if it change, tell CD update view
@@ -167,5 +196,36 @@ export class SignupPage {
 
   }
 
-  
+  postDate() {
+    var d = new Date();
+
+    if (d.getMinutes() < 10)
+      var min = "0" + d.getMinutes()
+    else
+      var min = d.getMinutes().toString()
+
+    if (d.getDate() < 10)
+      var ddate = "0" + d.getDate()
+    else
+      var ddate = d.getDate().toString()
+
+    if (d.getMonth() < 9)
+      var dmonth = "0" + (d.getMonth() + 1)
+    else
+      var dmonth = (d.getMonth() + 1).toString()
+
+    if (d.getHours() < 10)
+      var dhours = "0" + d.getHours()
+    else
+      var dhours = d.getHours().toString()
+
+    var showDate = +d.getDate() + "." + (d.getMonth() + 1) + "." + d.getFullYear() + " at " + d.getHours() + ":" + min
+
+    var dbDate = d.getFullYear() + "/" + dmonth + "/" + ddate + " at " + dhours + ":" + min
+
+    var date = [showDate, dbDate]
+    return date
+  }
+
+
 }
