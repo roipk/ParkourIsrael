ddd95966d99b990771cc6305fc305358d678dda1
Roi Madmon <roipk123@gmail.com>
2019-06-27 03:44:09 +0300

    fix posts

--- Pi/src/app/Pages/Post-Bubble/post-bubble.component.ts
+++ Pi/src/app/Pages/Post-Bubble/post-bubble.component.ts
@@ -11,11 +11,9 @@ import { equalSegments } from '@angular/router/src/url_tree';
 export class PostBubbleComponent implements OnInit {
 
   @Input() data: any
-  // @Input() user: any
   @ViewChild('img') img
-  @ViewChild('test') test
+  @ViewChild('content') content
   @ViewChild('src') profile
-show=true
   defaultAvatar = "https://firebasestorage.googleapis.com/v0/b/parkour-israel.appspot.com/o/images%2Favatar.jpg?alt=media&token=ec1dfd38-fa0d-4f73-a953-51e2c7756f5f"
 
   constructor(
@@ -23,37 +21,28 @@ show=true
   ) { }
 
   ngOnInit() {
-    if (this.data!=undefined &&this.data.file_name != '') {
-      this.show=true
+    if (this.data.file_name != '') {
       var storageRef = firebase.storage().ref()
       storageRef.child('images/' + this.data.file_name).getDownloadURL().then(res => {
         this.img.src = res
 
       })
       // {{data.content}}
-      
-      if(this.test!=undefined)
-       { this.test.el.innerHTML = this.data.show_message
+    }
+
+      this.content.el.innerHTML = this.data.show_message
       if (this.data.nameToShow == "צוות פארקור ישראל")
-      this.profile.src = this.defaultAvatar
+        this.profile.src = this.defaultAvatar
       else {
-        
         var storageRef = firebase.storage().ref()
         storageRef.child('ImageProfile/' + this.data.userProfile).getDownloadURL().then(res => {
           this.profile.src = res
         })
-        
       }
-    }
-    }
-    else
-    this.show=false
+
+
   }
-  
-getShow()
-{
-  return this.show
-}
+
   postPage() {
     this.router.navigateByUrl('/post-page')
   }
